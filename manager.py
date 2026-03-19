90b2fd57a8f7bf0c78acd1446ee09b788266a41c
gglvxx <andrei.cry.glv@gmail.com>
2026-03-19 16:54:44 +0200

    Pasul 2: Adaugat meniul principal si bucla programului

--- manager.py
+++ manager.py
@@ -1,20 +1,36 @@
 # manager.py
 
-# Definim parola master (ulterior o vom schimba cu ceva mai complex)
 PAROLA_MASTER = "student2024"
 
 def autentificare():
-    print("--- Manager de Parole ---")
-    incercare = input("Introdu parola master pentru a debloca seiful: ")
-    
-    if incercare == PAROLA_MASTER:
-        print("Acces permis!\n")
-        return True
-    else:
-        print("Parolă incorectă! Acces interzis.")
-        return False
+    print("\n=== SEIF DIGITAL v1.0 ===")
+    incercare = input("Introdu parola master: ")
+    return incercare == PAROLA_MASTER
 
-# Pornirea programului
-if autentificare():
-    # Aici va veni meniul principal mai târziu
-    print("Bun venit în sistem.")
+def afiseaza_meniu():
+    print("\n--- MENIU ---")
+    print("1. Adaugă o parolă nouă")
+    print("2. Vizualizează toate parolele")
+    print("3. Ieșire")
+    optiune = input("Alege o opțiune (1/2/3): ")
+    return optiune
+
+# Aici începe "Motorul" programului
+if __name__ == "__main__":
+    if autentificare():
+        print("Acces permis!")
+        
+        while True:
+            alegere = afiseaza_meniu()
+            
+            if alegere == "1":
+                print("\n[Logica pentru ADĂUGARE va fi aici]")
+            elif alegere == "2":
+                print("\n[Logica pentru VIZUALIZARE va fi aici]")
+            elif alegere == "3":
+                print("Se închide seiful. La revedere!")
+                break # Această comandă oprește bucla while și închide programul
+            else:
+                print("Opțiune invalidă, mai încearcă.")
+    else:
+        print("Acces interzis!")
