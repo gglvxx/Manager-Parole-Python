ad24173c2b0bb24d958fe2cf406a5cb75304cc38
gglvxx <andrei.cry.glv@gmail.com>
2026-03-19 17:14:15 +0200

    Pasul 5: Adaugat salvarea si incarcarea datelor din fisier JSON

--- manager.py
+++ manager.py
@@ -1,7 +1,27 @@
-# manager.py
+import json # Importăm biblioteca pentru lucrul cu fișiere JSON
+import os   # Ne ajută să verificăm dacă fișierul există deja
 
 PAROLA_MASTER = "student2024"
-seif_date = [] 
+NUME_FISIER = "date_parole.json"
+seif_date = []
+
+# --- FUNCȚII NOI PENTRU FIȘIERE ---
+
+def incarca_date():
+    global seif_date
+    if os.path.exists(NUME_FISIER):
+        with open(NUME_FISIER, "r") as f:
+            seif_date = json.load(f)
+            print("[INFO] Datele vechi au fost încărcate cu succes.")
+    else:
+        seif_date = []
+
+def salveaza_date():
+    with open(NUME_FISIER, "w") as f:
+        json.dump(seif_date, f, indent=4)
+    print("[INFO] Datele au fost salvate pe disk.")
+
+# ----------------------------------
 
 def autentificare():
     print("\n=== SEIF DIGITAL v1.0 ===")
@@ -16,38 +36,28 @@ def afiseaza_meniu():
     return input("Alege o opțiune (1/2/3): ")
 
 def adauga_parola():
-    site = input("Pentru ce site/aplicație? ")
-    user = input("Care este numele de utilizator? ")
-    parola = input("Care este parola? ")
-    
-    intrare = {"site": site, "user": user, "parola": parola}
-    seif_date.append(intrare)
-    print(f"\n[SUCCES] Datele pentru {site} au fost salvate!")
-
-# --- FUNCȚIA NOUĂ ---
+    site = input("Site: ")
+    user = input("User: ")
+    parola = input("Parola: ")
+    seif_date.append({"site": site, "user": user, "parola": parola})
+    salveaza_date() # Salvăm imediat ce am adăugat ceva nou!
 
 def vizualizeaza_parole():
-    if not seif_date: # Verificăm dacă lista este goală
-        print("\n[!] Seiful este gol. Nu ai nicio parolă salvată.")
+    if not seif_date:
+        print("\n[!] Seiful este gol.")
     else:
-        print("\n--- DATELE TALE SALVATE ---")
-        # Folosim o buclă 'for' pentru a trece prin fiecare dicționar din listă
         for element in seif_date:
             print(f"Site: {element['site']} | User: {element['user']} | Parola: {element['parola']}")
-        print("---------------------------")
-
-# --------------------
 
 if __name__ == "__main__":
     if autentificare():
+        incarca_date() # Încărcăm datele imediat după logare!
         while True:
             alegere = afiseaza_meniu()
             if alegere == "1":
                 adauga_parola()
             elif alegere == "2":
-                vizualizeaza_parole() # Apelăm funcția nouă
+                vizualizeaza_parole()
             elif alegere == "3":
-                print("Se închide seiful. La revedere!")
+                print("Se închide seiful...")
                 break
-            else:
-                print("Opțiune invalidă.")
